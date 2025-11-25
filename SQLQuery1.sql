71fb569a3254d9172461625cc72c73c816817bed
Aydan1703 <ildirimovaaydan3@gmail.com>
2025-11-25 13:20:37 +0400

    Add files via upload

---
+++ SQLQuery1.sql
@@ -0,0 +1,14 @@
+Create table Employees(
+EmployeeId int,
+FirstName varchar(20),
+LastName varchar(25),
+Email varchar(25),
+PhoneNumber varchar(20),
+HireDate datetime,
+JobId varchar(10),
+Salary int,
+CommissionPCT int,
+ManagerId int,
+DepartmentId int,
+)
+
